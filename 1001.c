d518fe1456ab4e0c86b7700b84a7688b8e1ef1e2
GONG <nomlasssssss@outlook.com>
2022-01-26 01:41:39 +0800

    <commit> initial commit

--- 1001.c
+++ 1001.c
@@ -1,1 +1,23 @@
-//
+#include <stdio.h>
+
+int main()
+{
+    int n = 0, count = 0;
+    scanf("%d", &n);
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            // even number
+            n /= 2;
+        }
+        else
+        {
+            //odd number
+            n = (3*n+1)/2;
+        }
+        count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
